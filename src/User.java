8d1ccc185e8f8a731c383d22056a24226dd9a6b0
elice-kim <grit_julia@naver.com>
2017-07-01 21:08:22 +0900

    making User Class implements Serializable

---
+++ src/User.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+/**
+ * Created by elicekim on 2017. 7. 1..
+ */
+public class User implements Serializable{
+
+    private String name;
+    //transient 로 전송하고싶지 않은 변수에 선언함
+    private transient String password;
+    private String email;
+    private int age;
+
+    public User(String name, String email, int age){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String toString(){
+        return "name :"+name+" password :"+ password +" email :"+email+" age :"+age;
+    }
+
+
+}
